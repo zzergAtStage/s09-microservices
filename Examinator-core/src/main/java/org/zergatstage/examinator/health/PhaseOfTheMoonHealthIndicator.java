9805654df04971705bb834ce1bf2016d0def753e
zzergAtStage <hightemplar98@iclod.com>
2024-03-03 03:22:33 +0100

    feat: api-gateway added

---
+++ Examinator-core/src/main/java/org/zergatstage/examinator/health/PhaseOfTheMoonHealthIndicator.java
@@ -0,0 +1,32 @@
+package org.zergatstage.examinator.health;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author father
+ */
+@Component
+public class PhaseOfTheMoonHealthIndicator implements HealthIndicator {
+    private Random random;
+
+    @PostConstruct
+    public void init() {
+        random = ThreadLocalRandom.current();
+    }
+
+    @Override
+    public Health health() {
+        if (random.nextInt(2) == 0) {
+            return Health.down()
+                    .status("Invalid phase of the Moon")
+                    .build();
+        }
+        return Health.up().build();
+    }
+}
